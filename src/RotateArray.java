66609304bb58a5746515f721d04f8166036e83ee
judge-mental <vishwas.vishi.24@gmail.com>
2018-02-05 23:11:04 +0530

    implemented contains duplicate problem

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -6,25 +6,42 @@ public class RotateArray {
 
     public static void main(String[] args) {
         RotateArray rotateArray = new RotateArray();
-        rotateArray.rotate(new int[]{1,2}, 1);
+        rotateArray.rotate(new int[]{1,2,3,4,5,6,7}, 3);
     }
 
     public void rotate(int[] nums, int k) {
-        int[] res = new int[nums.length];
-        if(k > nums.length) {
-            k = k % nums.length;
-        }
-        int rotateIdx = nums.length - k;
-        for(int i = 0; i < nums.length; i++) {
-            if(i < nums.length - k) {
-                res[i + k] = nums[i];
-            } else {
-                res[i - rotateIdx] = nums[i];
-            }
-        }
-        for(int i = 0; i < res.length; i++) {
-            nums[i] = res[i];
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
         }
     }
+
+//    public void rotate(int[] nums, int k) {
+//        int[] res = new int[nums.length];
+//        if(k > nums.length) {
+//            k = k % nums.length;
+//        }
+//        int rotateIdx = nums.length - k;
+//        for(int i = 0; i < nums.length; i++) {
+//            if(i < nums.length - k) {
+//                res[i + k] = nums[i];
+//            } else {
+//                res[i - rotateIdx] = nums[i];
+//            }
+//        }
+//        for(int i = 0; i < res.length; i++) {
+//            nums[i] = res[i];
+//        }
+//    }
 
 }
