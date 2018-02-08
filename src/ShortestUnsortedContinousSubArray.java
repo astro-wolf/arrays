5c92c79e31f48af268774f366545252e8483ff8b
judge-mental <vishwas.vishi.24@gmail.com>
2018-02-08 21:30:42 +0530

    implemented shortest unsorted sub array

---
+++ src/ShortestUnsortedContinousSubArray.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+/**
+ * Created by Vishwas on 07-02-2018.
+ */
+public class ShortestUnsortedContinousSubArray {
+
+    public static void main(String[] args) {
+        ShortestUnsortedContinousSubArray shortestUnsortedContinousSubArray = new ShortestUnsortedContinousSubArray();
+        int length = shortestUnsortedContinousSubArray.findUnsortedSubarray(new int[]{1, 3, 2, 3, 3});
+        System.out.print(length);
+    }
+
+    public int findUnsortedSubarray(int[] nums) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, i = -1, j = 0;
+        for(int l = 0, r = nums.length - 1; r >= 0; l++, r--) {
+            max = Math.max(max, nums[l]);
+            if(nums[l] != max)
+                i = l;
+            min = Math.min(min, nums[r]);
+            if(nums[r] != min)
+                j = r;
+        }
+        return i - j + 1;
+    }
+
+//    public int findUnsortedSubarray(int[] nums) {
+//        int left = 0, right = nums.length - 1;
+//        if(left == right)
+//            return 0;
+//        while (right > left) {
+//            if (nums[left] <= nums[left + 1])
+//                left++;
+//            else if (nums[right] > nums[right - 1])
+//                right--;
+//            else
+//                break;
+//        }
+//        if(left == right)
+//            return 0;
+//        return right + 1 - left;
+//    }
+}
